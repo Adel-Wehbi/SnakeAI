7ae21602142581e5608114cc47e0eca8661cce7e
jaredlueck <jared.lueck@hotmail.com>
2019-11-17 15:08:46 -0500

    added the offset for aesthetic

--- SnakeGame/view.py
+++ SnakeGame/view.py
@@ -5,10 +5,14 @@ import enum
 class GameView(object):
 
     grid = [
-            [CellContent.SnakeBody, CellContent.Food, CellContent.Food, CellContent.SnakeBody],
-            [CellContent.Food, CellContent.SnakeBody, CellContent.Empty, CellContent.Empty],
-            [CellContent.Empty, CellContent.Food, CellContent.SnakeBody, CellContent.Empty],
-            [CellContent.SnakeBody, CellContent.Food, CellContent.Food, CellContent.SnakeHead]
+            [CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody],
+            [CellContent.SnakeBody, CellContent.SnakeHead, CellContent.Empty, CellContent.Empty, CellContent.Empty, CellContent.Empty, CellContent.Empty, CellContent.Empty],
+            [CellContent.Empty, CellContent.Empty, CellContent.Food, CellContent.Food, CellContent.Food, CellContent.Empty, CellContent.Food, CellContent.Food],
+            [CellContent.Empty, CellContent.Empty, CellContent.Food, CellContent.Empty, CellContent.Empty, CellContent.Food, CellContent.Empty, CellContent.Empty],
+            [CellContent.Food, CellContent.Food, CellContent.Empty, CellContent.Empty, CellContent.Food, CellContent.Food, CellContent.Empty, CellContent.Food],
+            [CellContent.Empty, CellContent.Empty, CellContent.Food, CellContent.Empty, CellContent.Food, CellContent.Food, CellContent.Empty, CellContent.Empty],
+            [CellContent.Empty, CellContent.Food, CellContent.Empty, CellContent.Food, CellContent.Food, CellContent.Empty, CellContent.Food, CellContent.Food],
+            [CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody, CellContent.SnakeBody]
         ]
     
     BG_COLOR = green = 0, 135, 0
@@ -30,28 +34,24 @@ class GameView(object):
         for i in range(self.length):
             for j in range(self.width):
                 if(grid[i][j] == CellContent.Empty):
-                    pygame.draw.rect(self.screen, self.BG_COLOR, (self.getleftCoord(j), self.getTopCoord(i), self.cellWidth, self.cellLength))
+                    pygame.draw.rect(self.screen, self.BG_COLOR, (self.getleftCoord(j,0), self.getTopCoord(i,0), self.cellWidth, self.cellLength))
                 elif (grid[i][j] == CellContent.SnakeBody):
-                    pygame.draw.rect(self.screen, self.SNAKE_COLOUR, (self.getleftCoord(j)  + (self.cellWidth*0.05), self.getTopCoord(i) + (self.cellLength*0.05), self.cellWidth*0.9, self.cellLength*0.9))
+                    pygame.draw.rect(self.screen, self.SNAKE_COLOUR, (self.getleftCoord(j, 0.05), self.getTopCoord(i, 0.05), self.cellWidth*0.9, self.cellLength*0.9))
                 elif (grid[i][j] == CellContent.SnakeHead):
-                     pygame.draw.rect(self.screen, self.SNAKEHEAD_COLOR, (self.getleftCoord(j)  + (self.cellWidth*0.1), self.getTopCoord(i) + (self.cellLength*0.1), self.cellWidth*0.8, self.cellLength*0.8))
+                     pygame.draw.rect(self.screen, self.SNAKEHEAD_COLOR, (self.getleftCoord(j, 0.1), self.getTopCoord(i, 0.1), self.cellWidth*0.8, self.cellLength*0.8))
                 elif(grid[i][j] == CellContent.Food):
-                    center = self.getCenterCoord(j, i)
-                    pygame.draw.ellipse(self.screen, self.FOOD_COLOUR, (self.getleftCoord(j) + (self.cellWidth*0.25), self.getTopCoord(i) + (self.cellLength*0.25), (self.cellWidth)/2.0, (self.cellLength)/2.0))
+                    pygame.draw.ellipse(self.screen, self.FOOD_COLOUR, (self.getleftCoord(j, 0.25), self.getTopCoord(i, 0.25), (self.cellWidth)/2.0, (self.cellLength)/2.0))
         pygame.display.flip()          
                 
             
         
-    def getleftCoord(self, x):
-        return x * self.cellWidth
+    def getleftCoord(self, x, offset):
+        return x * self.cellWidth + (self.cellWidth*offset)
 
-    def getTopCoord(self, y):
-        return y * self.cellLength
-
-    def getCenterCoord(self, x, y):
-        return float(x * self.cellWidth + (self.cellWidth / 2.0)), float(y * self.cellLength + (self.cellLength / 2.0))
+    def getTopCoord(self, y, offset):
+        return y * self.cellLength + (self.cellLength*offset)
         
         
 if __name__ == '__main__':
-    x = GameView((4,4))
+    x = GameView((8,8))
     x.render(GameView.grid)
